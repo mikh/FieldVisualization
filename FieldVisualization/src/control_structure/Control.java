e11b73ec74d85da7fcf2d2a56824311a1731a9dc
Mikhail Andreev <mikha.andreev@gmail.com>
2014-12-30 18:47:57 -0500

    Initialized control structure.

---
+++ FieldVisualization/src/control_structure/Control.java
@@ -0,0 +1,9 @@
+package control_structure;
+
+public class Control {
+	public static void main(String[] args){
+		System.out.println(String.format("Starting FieldVisualizer v.%d.%d", Defines.MAJOR_VERSION, Defines.MINOR_VERSION));
+		
+		System.out.println("Finished Operations. Terminating.");
+	}
+}
---
+++ FieldVisualization/src/control_structure/Defines.java
@@ -0,0 +1,6 @@
+package control_structure;
+
+public class Defines {
+	public static final int MAJOR_VERSION = 0;
+	public static final int MINOR_VERSION = 0;
+}
