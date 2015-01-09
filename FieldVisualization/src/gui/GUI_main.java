7f1a3bd336d9a86ca35036aec204028aab64dffd
Mikhail Andreev <mikha.andreev@gmail.com>
2015-01-09 11:58:49 -0500

    Mouse tracking

--- FieldVisualization/src/gui/GUI_main.java
+++ FieldVisualization/src/gui/GUI_main.java
@@ -3,6 +3,8 @@ package gui;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Point;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 
@@ -10,6 +12,7 @@ import control_structure.Defines;
 
 public class GUI_main {
 	JFrame main_frame = null;
+	Tile[][] tiles = null;
 	
 	public GUI_main(){
 		/**** JFrame main_frame setup ****/
@@ -20,7 +23,7 @@ public class GUI_main {
 		main_frame.setPreferredSize(Defines.JFRAME_INITIAL_SIZE);
 		main_frame.setLocationRelativeTo(Defines.JFRAME_RELATIVE_COMPONENT_POSITION);
 		main_frame.setLayout(new GridLayout(Defines.JFRAME_NUMBER_OF_ROWS, Defines.JFRAME_NUMBER_OF_COLS));
-		
+		tiles = new Tile[Defines.JFRAME_NUMBER_OF_ROWS][Defines.JFRAME_NUMBER_OF_COLS];
 		populate_main_frame(Defines.JFRAME_NUMBER_OF_ROWS, Defines.JFRAME_NUMBER_OF_COLS, new Dimension(Defines.JFRAME_INITIAL_SIZE.width/Defines.JFRAME_NUMBER_OF_ROWS, Defines.JFRAME_INITIAL_SIZE.height/Defines.JFRAME_NUMBER_OF_COLS), Defines.TILE_INITIAL_COLOR);
 	
 		/**** Display GUI ****/
@@ -30,8 +33,82 @@ public class GUI_main {
 	private void populate_main_frame(int n_rows, int n_cols, Dimension size, Color color){
 		for (int ii = 0; ii < n_rows; ii++){
 			for(int jj = 0; jj < n_cols; jj++){
-				main_frame.add(new Tile(ii*n_cols+jj, size, color), ii, jj);
+				Tile t = new Tile(ii*n_cols+jj, size, color, new Point(ii,jj), this);
+				main_frame.add(t, ii, jj);
+				tiles[ii][jj] = t;
 			}
 		}
 	}
+	
+	public void echo_mouse_position(Point locus){
+		int max_iterations = 3, cur_iter = 1;
+		Color base_color = new Color(0,0,0), change = new Color((255-base_color.getRed())/max_iterations,(255-base_color.getGreen())/max_iterations,(255-base_color.getBlue())/max_iterations);
+		Color cur_color = new Color(change.getRed(), change.getGreen(), change.getBlue());
+		
+		ArrayList<Point> seen = new ArrayList<Point>();
+		ArrayList<Point> pointStack = new ArrayList<Point>();
+		ArrayList<Point> newPointStack = new ArrayList<Point>();
+		seen.add(locus);
+		pointStack.add(locus);
+		while(pointStack.size() != 0){
+			Point p = pointStack.get(0);
+			pointStack.remove(0);
+			for(int xx = -1; xx<2; xx++){
+				for(int yy = -1; yy < 2; yy++){
+					Point new_p = new Point(p.x+xx, p.y + yy);
+					if(!seen.contains(new_p)){
+						tiles[new_p.x][new_p.y].update_color(cur_color);
+						seen.add(new_p);
+						newPointStack.add(new_p);
+					}
+				}
+			}
+			if(pointStack.size() == 0){
+				if(cur_iter < max_iterations){
+					for(Point q : newPointStack){
+						pointStack.add(q);
+					}
+					newPointStack.clear();
+					cur_iter++;
+					cur_color = new Color(cur_color.getRed()+change.getRed(), cur_color.getGreen()+change.getGreen(), cur_color.getBlue()+change.getBlue());
+				}
+			}
+		}
+	}
+	
+	public void echo_mouse_absense(Point locus){
+		int max_iterations = 3, cur_iter = 1;
+		Color base_color = new Color(0,0,0), change = new Color((255-base_color.getRed())/max_iterations,(255-base_color.getGreen())/max_iterations,(255-base_color.getBlue())/max_iterations);
+		Color cur_color = new Color(255,255,255);
+		
+		ArrayList<Point> seen = new ArrayList<Point>();
+		ArrayList<Point> pointStack = new ArrayList<Point>();
+		ArrayList<Point> newPointStack = new ArrayList<Point>();
+		seen.add(locus);
+		pointStack.add(locus);
+		while(pointStack.size() != 0){
+			Point p = pointStack.get(0);
+			pointStack.remove(0);
+			for(int xx = -1; xx<2; xx++){
+				for(int yy = -1; yy < 2; yy++){
+					Point new_p = new Point(p.x+xx, p.y + yy);
+					if(!seen.contains(new_p)){
+						tiles[new_p.x][new_p.y].update_color(cur_color);
+						seen.add(new_p);
+						newPointStack.add(new_p);
+					}
+				}
+			}
+			if(pointStack.size() == 0){
+				if(cur_iter < max_iterations){
+					for(Point q : newPointStack){
+						pointStack.add(q);
+					}
+					newPointStack.clear();
+					cur_iter++;
+				}
+			}
+		}
+	}		
+	
 }
--- FieldVisualization/src/gui/Tile.java
+++ FieldVisualization/src/gui/Tile.java
@@ -2,9 +2,11 @@ package gui;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
@@ -13,9 +15,11 @@ public class Tile extends JPanel implements MouseListener{
 	private int ID = 0;
 	private Dimension size = null;
 	private Color color = Color.white;
+	private Point location = new Point(-1,-1);
+	private GUI_main parent_frame = null;
 	
 	/**** Class Construction ****/
-	public Tile(int ID, Dimension initial_size, Color initial_color){
+	public Tile(int ID, Dimension initial_size, Color initial_color, Point location, GUI_main parent_frame){
 		this.ID = ID;
 		this.size = initial_size;
 		this.setSize(size);
@@ -23,10 +27,21 @@ public class Tile extends JPanel implements MouseListener{
 		this.color = initial_color;
 		this.setBackground(color);
 		this.addMouseListener(this);
-		
+		this.location = location;
+		this.parent_frame = parent_frame;
+	}
+	
+	
+	/***Update Functions***/
+	public void update_color(Color new_color){
+		color = new_color;
+		this.setBackground(new_color);
+		this.repaint();
+		this.revalidate();
 	}
 	
 	/***Dimensional Overrides***/
+	
 	@Override
 	public Dimension getMinimumSize(){
 		return size;
@@ -50,6 +65,7 @@ public class Tile extends JPanel implements MouseListener{
 		this.setBackground(Color.BLACK);
 		this.repaint();
 		this.revalidate();
+		this.parent_frame.echo_mouse_position(this.location);
 	}
 
 	@Override
@@ -58,6 +74,7 @@ public class Tile extends JPanel implements MouseListener{
 		this.setBackground(Color.WHITE);
 		this.repaint();
 		this.revalidate();
+		/this.parent_frame.echo_mouse_absense(this.location);
 	}
 
 	@Override
